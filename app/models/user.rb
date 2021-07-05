104fdcfaee7b4c6d1391d376a165274adaa67b87
vishakha011 <khatadevishakha01@gmail.com>
2021-07-05 19:51:49 +0530

    Created User model

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+
+
+  validates :fist_name, presence: true, length: {maximum: 50}
+  validates :last_name, presence: true, length: {maximum: 50}
+  validates :email, presence: true,
+                    uniqueness: true,
+                    length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX }
+  
+  before_save :to_lowercase
+
+  private
+
+    def to_lowercase
+      email.downcase!
+    end
+end
