2feff7dd1804629e0af8a76b776f80789bde7a6a
vishakha011 <khatadevishakha01@gmail.com>
2021-07-19 08:34:24 +0530

    Removed before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
 include CurrentUserConcern
 
   skip_before_action :verify_authenticity_token
-  before_action :check_current_user
 
   private
     def check_current_user
